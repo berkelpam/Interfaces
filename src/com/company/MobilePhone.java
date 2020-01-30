f8332d450bf2775071cf0b8284c87b5723d87f15
Paul <p.a.m.van.berkel@gmail.com>
2020-01-30 21:03:10 +0100

    init mobile phone

---
+++ src/com/company/MobilePhone.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class MobilePhone implements ITelephone {
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn=false;
+
+    @Override
+    public void powerOn() {
+        isOn=true;
+        System.out.println("Mobile phone turned on.");
+
+    }
+
+    @Override
+    public void dail(int phoneNumber) {
+        if (isOn){
+        System.out.println("Now ringing" + phoneNumber + "on mobile phone.");}
+        else
+        {
+            System.out.println("Phone is switched off.");
+        }
+
+    }
+
+    @Override
+    public void anser() {
+
+            if (isRinging) {
+                System.out.println("Answering the mobile phone");
+                isRinging = false;
+            }
+
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber==myNumber && isOn){
+            isRinging =true;
+            System.out.println("Melody playing");
+        }
+        else{
+            isRinging=false;
+        }
+        return isRinging;
+    }
+
+    public  MobilePhone (int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
+}
